63d6a2a857327c5870bdbe99e7cf50b2a99b5967
wellingtonhm <wellington.hm.w@gmail.com>
2022-05-03 14:16:37 -0300

    Implantação

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+    View,
+    Text,
+    Image,
+    StatusBar
+} from 'react-native'
+
+import { ButtonIcon } from "../../components/ButtonIcon";
+import IlustrationImg from '../../assets/illustration.png'
+import { styles } from './styles'
+
+export function Signin() {
+    return (
+        <View style={styles.container}>
+            <Image
+                source={IlustrationImg}
+                style={styles.image}
+                resizeMode="stretch" />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Conecte-se {'\n'}
+                    e organize suas {'\n'}
+                    jogatinas
+                </Text>
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {'\n'}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon
+                    title="Entrar com Discord"
+                    activeOpacity={0.7}
+                />
+
+            </View>
+
+        </View>
+    );
+}
